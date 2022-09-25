a0a1e49dd7ec4ca092adbd94f3dcb12969d9d26d
장윤희 <dbsgml9325@etoos.com>
2022-09-25 18:51:37 +0900

    module error

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
 
 ReactDOM.render(
   <React.StrictMode>
